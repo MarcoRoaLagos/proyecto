1786bec164e932511c44b9e4063e1e5023493c62
paulamikal <paula.pinto11@inacapmail.cl>
2024-11-05 00:00:17 -0300

    trabaja con nosotros y registro. bdd

--- allsnow_app/forms.py
+++ allsnow_app/forms.py
@@ -1,37 +1,63 @@
 from django import forms
-from .models import SolicitudTienda
-from .models import SolicitudUsuario
+from .models import SolicitudUsuario, SolicitudLocal, Registrarme
 from .models import Inventario
 from .models import InventarioArriendo
 
-class SolicitudTiendaForm(forms.ModelForm):
-    class Meta:
-        model = SolicitudTienda
-        fields = ['nombre_tienda', 'patente_local', 'region']
+
+####PAULA MIKAL INICIO####
 
 class SolicitudUsuarioForm(forms.ModelForm):
     class Meta:
         model = SolicitudUsuario
-        fields = ['nombre', 'apellido', 'rut', 'telefono', 'correo', 'conf_correo', 'contraseña', 'conf_contraseña']
+        fields = ['nombre', 'apellido', 'rut', 'telefono', 'correo', 'contraseña']
         widgets = {
             'contraseña': forms.PasswordInput(),
-            'conf_contraseña': forms.PasswordInput(),
         }
 
-    def clean(self):
-        cleaned_data = super().clean()
-        correo = cleaned_data.get("correo")
-        conf_correo = cleaned_data.get("conf_correo")
-        contraseña = cleaned_data.get("contraseña")
-        conf_contraseña = cleaned_data.get("conf_contraseña")
 
-        if correo and conf_correo and correo != conf_correo:
-            self.add_error('conf_correo', "Los correos deben coincidir.")
+class SolicitudLocalForm(forms.ModelForm):
+    REGION_CHOICES = [
+        ('Arica y Parinacota', 'Arica y Parinacota'),
+        ('Tarapacá', 'Tarapacá'),
+        ('Antofagasta', 'Antofagasta'),
+        ('Atacama', 'Atacama'),
+        ('Coquimbo', 'Coquimbo'),
+        ('Valparaíso', 'Valparaíso'),
+        ('Metropolitana de Santiago', 'Metropolitana de Santiago'),
+        ('O’Higgins', 'O’Higgins'),
+        ('Maule', 'Maule'),
+        ('Ñuble', 'Ñuble'),
+        ('Biobío', 'Biobío'),
+        ('La Araucanía', 'La Araucanía'),
+        ('Los Ríos', 'Los Ríos'),
+        ('Los Lagos', 'Los Lagos'),
+        ('Aysén', 'Aysén'),
+        ('Magallanes', 'Magallanes'),
+    ]
 
-        if contraseña and conf_contraseña and contraseña != conf_contraseña:
-            self.add_error('conf_contraseña', "Las contraseñas deben coincidir.")
+    ubicacion = forms.ChoiceField(choices=REGION_CHOICES)
 
-        return cleaned_data
+    class Meta:
+        model = SolicitudLocal
+        fields = ['nombre_local', 'patente', 'ubicacion']
+
+
+
+class RegistrarmeForm(forms.ModelForm):
+    class Meta:
+        model = Registrarme
+        fields = ['nombre', 'telefono', 'correo', 'contraseña']
+        widgets = {
+            'contraseña': forms.PasswordInput(), } 
+        
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Correo electrónico', max_length=254)
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+
+####PAULA MIKAL FIN####
+
+
+
 
 class InventarioForm(forms.ModelForm):
     class Meta:
